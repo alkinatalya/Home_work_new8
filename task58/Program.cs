37545ef576d35a0382c90364f26de5b3d6af9e12
Natalya <alkinatalya@yandex.com>
2022-12-02 09:42:51 +0300

    add task58

---
+++ task58/Program.cs
@@ -0,0 +1,57 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет 
+находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+int[,] firstMartrix = GetArrayRandom(2, 2);
+Console.WriteLine("Первая матрица:");
+PrintArray(firstMartrix);
+int[,] secomdMartrix = GetArrayRandom(2, 2);
+Console.WriteLine("Вторая матрица:");
+PrintArray(secomdMartrix);
+int[,] resultMatrix = new int[2, 2];
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine("Произведение первой и второй матриц:");
+PrintArray(resultMatrix);
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum += firstMartrix[i, k] * secomdMartrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+}
+int[,] GetArrayRandom(int rows, int cols, int minValue = 0, int maxValue = 9)
+{
+    int[,] array = new int[rows, cols];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
